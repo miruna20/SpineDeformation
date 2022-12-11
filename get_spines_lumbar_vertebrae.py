ee240fded41c5a816af1d3711ba62ece63eefb62
Miruna <miruna.gafencu@tum.de>
2022-12-11 17:04:26 +0100

    ADDED: script that generates a txt file with all spines that contain all lumbar vertebrae

--- get_spines_lumbar_vertebrae.py
+++ get_spines_lumbar_vertebrae.py
@@ -1,36 +1,69 @@
 import glob
 import os
 import json
+import argparse
+from pathlib import Path
 
 def get_spines_with_lumbar_vertebrae(root_folder, file):
+
+    """
+    Generate a .txt file with the names of all spines that contain all lumbar vertebrae
+    :param root_folder: root folder of the spine folders
+    :param file: path of txt file to be saved
+    :return:
+    """
+
     # gather all of the json files to be able to check for lumbar vertebrae
+    filenames = []
+    for path in Path(os.path.join(root_folder)).rglob('*.json'):
+        filenames.append(str(path))
 
-    #TODO get here the version that looks recursively for files
-    filenames_json = glob(os.path.join(root_folder, '*.json'))
-
-    lumbar_vertebreae_file = open(file, "w")
-
+    lumbar_spines_file = open(file, "w")
 
     # iterate over them and check if they have L1 until L5 present
     lumbar_labels = [20,21,22,23,24]
-    for json_file in filenames_json:
+    for json_file in filenames:
         contained_lumbar_vert = []
         f = open(json_file)
         data = json.load(f)
-        for i in range(len(data)):
+        for i in range(1,len(data)):
             if 20<= data[i]["label"] <= 24:
                 contained_lumbar_vert.append(data[i]["label"])
 
         # if all lumbar vertebrae are contained
         if contained_lumbar_vert == lumbar_labels:
-            lumbar_vertebreae_file.write(os.path.basename(json_file))
-
-
-
-
-    # if they do then write the name of the folder (e.g sub-verse90) into the list
+            path_to_json_file = os.path.dirname(json_file)
+            lumbar_spines_file.write(os.path.basename(path_to_json_file))
+            lumbar_spines_file.write("\n")
 
 
 if __name__ == "__main__":
+
+    # example setup
+    """
     root_folder = "/home/miruna20/Documents/Thesis/Dataset/VerSe2020/full_spines/subjectbased_structure/01_training"
     file = "../samples/lumbar_spines.txt"
+    
+    """
+    arg_parser = argparse.ArgumentParser(description="Generate txt file with the names of the spines that contain all lumbar vertebrae")
+
+    arg_parser.add_argument(
+        "--root_folder",
+        required=True,
+        dest="root_folder",
+        help="Root path to the vertebrae folders."
+    )
+
+    arg_parser.add_argument(
+        "--txt_file_lumbar_spines",
+        required=True,
+        dest="txt_file",
+        help="File where the names of the lumbar vertebrae will be written"
+    )
+
+    args = arg_parser.parse_args()
+
+    get_spines_with_lumbar_vertebrae(root_folder=args.root_folder, file=args.txt_file)
+
+
+
